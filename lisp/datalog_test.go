112587a38b155a48362a96fcb9dfbb33ca3c0f2f
Sjoerd Dost <deosjr@gmail.com>
2023-05-23 20:48:56 +0200

    datalog optimisation using entity/attr indices

--- lisp/datalog_test.go
+++ lisp/datalog_test.go
@@ -156,3 +156,127 @@ func TestDatalogFixpoint(t *testing.T) {
 		}
 	}
 }
+
+func TestDatalogImpure(t *testing.T) {
+	main := newProcess()
+	env := GlobalEnv()
+	loadKanren(main, env)
+	loadDatalog(main, env)
+	for i, tt := range []struct {
+		input  string
+		want   string
+		sorted bool
+	} {
+        // first, lets check if we can figure out if a var is bound or not
+        // NOTE: this breaks logical purity; we can no longer freely reorder goals
+        {
+            input: `(define boundo (lambda (v)
+                (lambda (s/c)
+                  (if (var? v)
+                    (let ((x (walk v (car s/c))))
+                      (if (var? x) mzero (unit s/c)))
+                    (unit s/c))
+            )))`,
+        },
+        {
+            input: "(run* (fresh (q x) (equalo q x) (boundo x) (equalo x 42)))",
+            want:  "()",
+        },
+        {
+            input: "(run* (fresh (q x) (equalo q x) (equalo x 42) (boundo x)))",
+            want:  "(42)",
+        },
+        {
+            input: `(define unboundo (lambda (v)
+                (lambda (s/c)
+                  (if (var? v)
+                    (let ((x (walk v (car s/c))))
+                      (if (var? x) (unit s/c) mzero))
+                    mzero)
+            )))`,
+        },
+        // then lets write a relation using conde and var checking
+        {
+            input: `(define varswitcho (lambda (x y)
+                      (conde 
+                        [(boundo x) (equalo y 1)]
+                        [(unboundo x) (equalo y 2)])))`,
+        },
+        {
+            input: "(run* (fresh (q x y) (equalo q y) (varswitcho x y) (equalo x 42)))",
+            want:  "(2)",
+        },
+        {
+            input: "(run* (fresh (q x y) (equalo q y) (equalo x 42) (varswitcho x y)))",
+            want:  "(1)",
+        },
+        // binds y to whatever v is bound to, or 'unbound if not bound
+        {
+            input: `(define boundo (lambda (v w)
+                (lambda (s/c)
+                  (if (var? v)
+                    (let ((x (walk v (car s/c))))
+                      (if (var? x) ((equalo w 'unbound) s/c) ((equalo w x) s/c)))
+                    ((equalo w v) s/c))
+            )))`,
+        },
+        {
+            input: "(run* (fresh (q x y) (equalo q y) (boundo x y) (equalo x 42)))",
+            want:  "(unbound)",
+        },
+        {
+            input: "(run* (fresh (q x y) (equalo q y) (equalo x 42) (boundo x y)))",
+            want:  "(42)",
+        },
+        // after that, we can write smth like 'if (car tuple) is bound, match against entry_index'
+        // NOTE: looking a value up in a hashmap like this: definitely not logically pure!
+        {
+            input: "(define hm (make-hashmap))",
+        },
+        {
+            input: "(hashmap-set! hm 1 2)",
+        },
+        {
+            input: `(define lookupo (lambda (m key value)
+                (lambda (s/c)
+                  (let ((k 
+                    (if (var? key)
+                      (walk key (car s/c))
+                      key)))
+                    (let ((v (hashmap-ref m k #f))) (if v ((equalo value v) s/c) mzero))
+            ))))`,
+        },
+        {
+            input: "(run* (fresh (q x) (equalo q x) (lookupo hm 1 x)))",
+            want:  "(2)",
+        },
+        {
+            input: "(run* (fresh (q x y) (equalo q y) (equalo x 1) (lookupo hm x y)))",
+            want:  "(2)",
+        },
+        {
+            input: "(run* (fresh (q x y) (equalo q y) (lookupo hm x y) (equalo x 1)))",
+            want:  "()",
+        },
+	} {
+		p, err := parse(tt.input)
+		if err != nil {
+			t.Errorf("%d) parse error %v", i, err)
+		}
+		e, err := main.evalEnv(env, p)
+		if err != nil {
+			t.Errorf("%d) eval error %v", i, err)
+		}
+		if tt.sorted {
+			list := cons2list(e.AsPair())
+			sort.Slice(list, func(i, j int) bool {
+				return list[i].AsNumber() < list[j].AsNumber()
+			})
+			e = list2cons(list...)
+		}
+		got := e.String()
+		if got != tt.want {
+			t.Errorf("%d) got %s want %s", i, got, tt.want)
+		}
+	}
+}
